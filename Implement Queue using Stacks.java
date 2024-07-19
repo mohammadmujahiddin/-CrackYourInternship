31592b24590cebb5a9c63a3c29a1d9fa1a08baf5
Mohammad Mujahiddin Shaik <129599522+mohammadmujahiddin@users.noreply.github.com>
2024-07-19 23:26:10 +0530

    15

---
+++ Implement Queue using Stacks.java
@@ -0,0 +1,55 @@
+// 232. Implement Queue using Stacks
+// Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+// Implement the MyQueue class:
+// void push(int x) Pushes element x to the back of the queue.
+// int pop() Removes the element from the front of the queue and returns it.
+// int peek() Returns the element at the front of the queue.
+// boolean empty() Returns true if the queue is empty, false otherwise.
+// Notes:
+// You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
+// Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+
+
+class MyQueue {
+    Stack<Integer> s1;
+    Stack<Integer> s2;
+    int front = 0;
+
+    public MyQueue() {
+        this.s1 = new Stack<>();
+        this.s2 = new Stack<>();        
+    }
+    
+    public void push(int x) {
+        if(s1.empty()){
+            front = x;
+        }
+        s1.push(x);
+    }
+    
+    public int pop() {
+        if(s2.isEmpty()){
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+        }
+        return s2.pop();        
+    }
+    
+    public int peek() {
+        return s2.isEmpty() ? front:s2.peek();        
+    }
+    
+    public boolean empty() {
+        return s1.empty() && s2.empty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
