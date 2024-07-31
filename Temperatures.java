fde1cf607a9b3940d54f99e10fc26f1ed1445b9e
Mohammad Mujahiddin Shaik <129599522+mohammadmujahiddin@users.noreply.github.com>
2024-07-31 18:49:17 +0530

    😍

---
+++ Temperatures.java
@@ -0,0 +1,21 @@
+739. Daily Temperatures
+Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is 
+the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int prevIndex = stack.pop();
+                answer[prevIndex] = i - prevIndex;
+            }
+            stack.push(i);
+        }
+        return answer;
+    }
+}
