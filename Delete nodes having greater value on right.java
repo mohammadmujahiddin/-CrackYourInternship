26e7e9fd3e0270fb3303afc14a6cd867db48d9d1
Mohammad Mujahiddin Shaik <129599522+mohammadmujahiddin@users.noreply.github.com>
2024-08-05 21:14:38 +0530

    ↻

--- Delete nodes having greater value on right.java
+++ Delete nodes having greater value on right.java
@@ -8,7 +8,7 @@ class Solution
     Node compute(Node head)
     {
         // your code here
-        if(head.next == null) return null;
+        if(head.next == null) return head;
         Node connect = compute(head.next);
         
         if(head.data >= connect.data){
