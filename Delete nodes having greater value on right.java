ff90b2c2e19fd8d18f419582bbfdab1058985219
Mohammad Mujahiddin Shaik <129599522+mohammadmujahiddin@users.noreply.github.com>
2024-08-05 20:58:19 +0530

    Create Delete nodes having greater value on right.java

---
+++ Delete nodes having greater value on right.java
@@ -0,0 +1,20 @@
+Delete nodes having greater value on right
+Given a singly linked list, remove all the nodes in the list which have any node on their right whose value is greater. 
+(Not just immediate Right , but entire List on the Right)
+
+
+class Solution
+{
+    Node compute(Node head)
+    {
+        // your code here
+        if(head.next == null) return null;
+        Node connect = compute(head.next);
+        
+        if(head.data >= connect.data){
+            head.next = connect;
+            return head;
+        }
+        return connect;
+    }
+}
